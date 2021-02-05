6eae3d1b49f62cb46c7bb41db993cc9ee70f9689
Minura Kariyawasam <minura.20191020@iit.ac.lk>
2021-02-05 11:15:15 +0530

    build: project structure
    create folder structure for fornt-end, back-end and the deep learning parts.

--- constants/index.js
+++
@@ -1,5 +0,0 @@
-import icons from "./icons";
-import images from "./images";
-import appTheme, { colours, cntSizes, appFonts } from "./appTheme";
-
-export { icons, images, appTheme, colours, cntSizes, appFonts };
