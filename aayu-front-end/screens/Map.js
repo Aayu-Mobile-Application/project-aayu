780f9cc6939b8f0745555771d7578888fd658454
Minura Kariyawasam <minura.20191020@iit.ac.lk>
2021-02-05 21:18:38 +0530

    build: Map screen

---
+++ aayu-front-end/screens/Map.js
@@ -0,0 +1,12 @@
+import React from "react";
+import{ View, Text } from "react-native";
+
+const Map = () => {
+    return(
+        <View>
+            <Text>Map Info</Text>
+        </View>
+    )
+}
+
+export default Map;
--- aayu-front-end/screens/index.js
+++ aayu-front-end/screens/index.js
@@ -1,10 +1,12 @@
 import HomeScreen from './HomeScreen'
 import PlantInfo from './PlantInfo'
 import Scan from './Scan'
+import Map from './Map'
 
 
 export{
     HomeScreen,
     PlantInfo,
-    Scan
+    Scan,
+    Map
 }
