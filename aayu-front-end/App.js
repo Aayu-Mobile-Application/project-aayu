361e63bebe0dcefecd02ccc88c49d5181eb3f1f4
Minura Kariyawasam <minura.20191020@iit.ac.lk>
2021-02-09 22:14:15 +0530

    fix: sum conflicts before merged.
    aayu-front-end/App.js
    aayu-front-end/screens/index.js

--- aayu-front-end/App.js
+++ aayu-front-end/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import appNavigTabs from "./navigation/appNavTabs";
-import { HomeScreen, PlantInfo } from "./screens";
+import { HomeScreen, PlantInfo, AppLoginScreen } from "./screens";
 
 const Stack = createStackNavigator();
 
@@ -14,7 +14,10 @@ const App = () => {
           headerShown: false,
         }}
         initialRouterName={"Main"}
+        initialRouterName={"appSignUp"} //setting up the first screen to be displayed, SignUp
       >
+         {/* navigation between tabs */}
+        <Stack.Screen name="appSignUp" component={AppLoginScreen}/>
         <Stack.Screen name="Home" component={appNavigTabs} />
         <Stack.Screen name="PlantInfo" component={PlantInfo} />
       </Stack.Navigator>
--- aayu-front-end/screens/index.js
+++ aayu-front-end/screens/index.js
@@ -2,11 +2,12 @@ import HomeScreen from './HomeScreen'
 import PlantInfo from './PlantInfo'
 import Scan from './Scan'
 import Map from './Map'
-
+import AppLoginScreen from './AppLoginScreen'
 
 export{
     HomeScreen,
     PlantInfo,
     Scan,
-    Map
+    Map,
+    AppLoginScreen
 }
