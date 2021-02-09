fac4fb04555e599e35e76eddecd22c4890f4aa4b
ArunaRandika <randiikaroxxs98@gmail.com>
2021-02-09 22:58:24 +0530

    MapView has been added to the main branch
    Co-Authored-By: Minura Kariyawasam <66254409+MinuraKariyawasam@users.noreply.github.com>
    Co-Authored-By: Adeepa Jayawardana <66461199+Adee99@users.noreply.github.com>

--- aayu-front-end/App.js
+++ aayu-front-end/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import {createStackNavigator} from "@react-navigation/stack";
-import {NavigationContainer} from "@react-navigation/native";
-import appNavigTabs from './navigation/appNavTabs';
-import {HomeScreen,PlantInfo,AppLoginScreen} from './screens';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import appNavigTabs from "./navigation/appNavTabs";
+import { HomeScreen, PlantInfo, AppLoginScreen } from "./screens";
+import Map from "./screens/Map";
 
 const Stack = createStackNavigator();
 
-const App =() =>{
-  return(
+const App = () => {
+  return (
     <NavigationContainer>
-        <Stack.Navigator
-            screenOptions={{
-                headerShown: false
-            }}
-            initialRouterName={"appSignUp"} //setting up the first screen to be displayed, SignUp
-          >
-            {/* navigation between tabs */}
-          <Stack.Screen name="appSignUp" component={AppLoginScreen}/>
-          <Stack.Screen name="HomeScreen" component={appNavigTabs} />
-          <Stack.Screen name="PlantInfo" component={PlantInfo} />
-        </Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouterName={"appSignUp"} //setting up the first screen to be displayed, SignUp
+      >
+        {/* navigation between tabs */}
+        <Stack.Screen name="appSignUp" component={AppLoginScreen} />
+        <Stack.Screen name="HomeScreen" component={appNavigTabs} />
+        <Stack.Screen name="PlantInfo" component={PlantInfo} />
+        <Stack.Screen name="MapView" component={Map} />
+      </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
 export default App;
