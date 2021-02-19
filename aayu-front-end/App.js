0b800e0fa05fa6636fe4c8e73541b6aef8cffc7c
Adee99 <adee99@users.noreply.github.com>
2021-02-19 21:32:22 +0530

    Revert "Merge branch 'main' of https://github.com/Aayu-Mobile-Application/project-aayu into main"
    This reverts commit e4e5684238d76e368ca80be9f1418e5c4b760fa8, reversing
    changes made to e166003fa22a34aeb1f7ded10530006f5eabcf29.

--- aayu-front-end/App.js
+++ aayu-front-end/App.js
@@ -1,29 +1,27 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
-import appNavigTabs from "./navigation/appNavTabs";
-import { HomeScreen, PlantInfo, AppLoginScreen } from "./screens";
-import Map from "./screens/Map";
+import React from 'react';
+import {createStackNavigator} from "@react-navigation/stack";
+import {NavigationContainer} from "@react-navigation/native";
+import appNavigTabs from './navigation/appNavTabs';
+import {HomeScreen,PlantInfo,AppLoginScreen} from './screens';
 
 const Stack = createStackNavigator();
 
-const App = () => {
-  return (
+const App =() =>{
+  return(
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-        initialRouterName={"appSignUp"} //setting up the first screen to be displayed, SignUp
-      >
-        {/* navigation between tabs */}
-        <Stack.Screen name="appSignUp" component={AppLoginScreen} />
-        <Stack.Screen name="HomeScreen" component={appNavigTabs} />
-        <Stack.Screen name="PlantInfo" component={PlantInfo} />
-        <Stack.Screen name="MapView" component={Map} />
-      </Stack.Navigator>
+        <Stack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}
+            initialRouterName={"appSignUp"} //setting up the first screen to be displayed, SignUp
+          >
+            {/* navigation between tabs */}
+          <Stack.Screen name="appSignUp" component={AppLoginScreen}/>
+          <Stack.Screen name="HomeScreen" component={appNavigTabs} />
+          <Stack.Screen name="PlantInfo" component={PlantInfo} />
+        </Stack.Navigator>
     </NavigationContainer>
-  );
-};
+  )
+}
 
 export default App;
