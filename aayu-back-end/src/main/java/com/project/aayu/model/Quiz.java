196bd4a21fe2b17601395c434ed60eb175c41f2f
Minura Kariyawasam <minura.20191020@iit.ac.lk>
2021-02-19 17:26:12 +0530

    build: quiz class in model
    add basic pojo functionalities

--- aayu-back-end/src/main/java/com/project/aayu/model/Quiz.java
+++ aayu-back-end/src/main/java/com/project/aayu/model/Quiz.java
@@ -1,4 +1,39 @@
 package com.project.aayu.model;
 
 public class Quiz {
+
+    private String user; // change to user type
+    private int quizScore;
+
+    // define parameterize constructor
+    public Quiz(String user, int quizScore) {
+        this.user = user;
+        this.quizScore = quizScore;
+    }
+
+    // define getters and setters
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public int getQuizScore() {
+        return quizScore;
+    }
+
+    public void setQuizScore(int quizScore) {
+        this.quizScore = quizScore;
+    }
+
+    // define toString method
+    @Override
+    public String toString() {
+        return "Quiz{" +
+                "user='" + user + '\'' +
+                ", quizScore=" + quizScore +
+                '}';
+    }
 }
