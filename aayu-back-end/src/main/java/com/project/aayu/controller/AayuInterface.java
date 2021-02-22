898591f73472a9f05e77b011a783eff45837dd8f
Minura Kariyawasam <minura.20191020@iit.ac.lk>
2021-02-22 21:37:06 +0530

    create: aayu interface
    create aayu interface and add basic methods what will implement on the aayu manager.

---
+++ aayu-back-end/src/main/java/com/project/aayu/controller/AayuInterface.java
@@ -0,0 +1,26 @@
+package com.project.aayu.controller;
+
+import com.project.aayu.model.Map;
+import com.project.aayu.model.Plant;
+import com.project.aayu.model.Quiz;
+import com.project.aayu.model.User;
+
+import java.util.List;
+
+public interface AayuInterface {
+    // add location to the database
+    void addLocation(double longitude,int latitude, String userName, String plantName);
+    // get location details
+    List<Map> viewLocation();
+    // add quiz score
+    void addScore(int score, String userName);
+    // view quiz score
+    List<Quiz> viewScore();
+    // add new user
+    void addNewUser(int userId, String userName, String userEmail);
+    // view user details
+    List<User> viewUser();
+    // view plant details
+    List<Plant> viewPlant();
+
+}
