254f96aa6fceba70f50fdd6c3fa01147910f79f1
Adee99 <66461199+Adee99@users.noreply.github.com>
2021-03-17 14:48:59 +0530

    update : login page with navigation

--- aayu-front-end-java/Aayu/app/src/main/java/com/mindscape/aayu/AppLogin.java
+++ aayu-front-end-java/Aayu/app/src/main/java/com/mindscape/aayu/AppLogin.java
@@ -2,13 +2,33 @@ package com.mindscape.aayu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AppLogin extends AppCompatActivity {
+    //login btn
+    ImageView loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_login2);
+
+        //getByBtnId
+        loginBtn = findViewById(R.id.loginBtn);
+
+
+        //set intent from main to scan
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 }
